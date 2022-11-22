ef865cf5f302e1db308f2016553bd7283fa90d7b
Vítor Ambrizzi <ambrizzivitor@gmail.com>
2022-11-22 17:47:24 -0300

    Started working on search input

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,29 @@ import { Link, NavLink } from 'react-router-dom'
 import styled from 'styled-components'
 
 const Header = () => {
+
+  const Search = async (value) => {
+    const response = await fetch(`https://api.spotify.com/v1/search?q=${value}&type=album`)
+    const result = await response.json()
+    console.log(result)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const search = event.target.search.value
+    Search(search)
+  }
+
   return (
     <Background>
       <Container>
         <Link to='/'>Social Music</Link>
+        <div>
+          <form onSubmit={(event) => handleSubmit(event)}>
+            <input type='text' name='search' placeholder='Search'></input>
+            <button type='submit'>Send</button>
+          </form>
+        </div>  
         <nav>
           <ul>
             <li><NavLink to='/user'>User</NavLink></li>
