848c346cc0697be7aaef6eee22e04b7531179caa
Vítor Ambrizzi <ambrizzivitor@gmail.com>
2022-11-28 18:37:06 -0300

    Created a getToken helper

---
+++ src/helpers/spotify.js
@@ -0,0 +1,22 @@
+import {CLIENT_ID, CLIENT_SECRET, SPOTIFY_ACCOUNTS} from '../config'
+
+const getToken = async () => {
+  const Buffer = require('buffer/').Buffer
+
+  const requestOptions = {
+    method: 'POST',
+    body: new URLSearchParams('grant_type=client_credentials'),
+    headers: {
+      'Authorization': 'Basic ' + new Buffer(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64'),
+      'Content-type': 'application/x-www-form-urlencoded',
+      'Accept': 'application/json'
+    }
+  }
+
+  const response = await fetch(SPOTIFY_ACCOUNTS, requestOptions)
+  const result = await response.json()
+
+  return result.access_token  
+}
+
+export default getToken
--- src/pages/Spotify.js
+++ src/pages/Spotify.js
@@ -1,28 +1,11 @@
-import {CLIENT_ID, CLIENT_SECRET, SPOTIFY_ACCOUNTS} from '../config'
+import getToken from '../helpers/spotify'
 
 const Spotify = () => {
-  const Buffer = require('buffer/').Buffer
-  
-  const options = {
-    method: 'POST',
-    body: new URLSearchParams('grant_type=client_credentials'),
-    headers: {
-      'Authorization': 'Basic ' + new Buffer(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64'),
-      'Content-type': 'application/x-www-form-urlencoded',
-      'Accept': 'application/json'
-    }
-  }
-
-  const request = async () => {
-    const response = await fetch(SPOTIFY_ACCOUNTS, options)
-    const result = await response.json()
-    console.log(result)
-  }
 
   return (
     <>
       <h1>Spotify</h1>
-      <button onClick={() => request()} >Manda essa porra</button>
+      <button onClick={() => console.log(getToken())} >Manda essa porra</button>
     </>
   )
 }
