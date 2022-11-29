d293c402c2af1a53622954ad894c2691e084bd5d
Vítor Ambrizzi <ambrizzivitor@gmail.com>
2022-11-28 21:01:13 -0300

    Removed client id

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // Spotify API
 export const SPOTIFY_API = 'https://api.spotify.com/v1'
 export const SPOTIFY_ACCOUNTS = 'https://accounts.spotify.com/api/token'
-export const CLIENT_ID = 'b5a62d946c224d10a031a1f783f45ac6'
+export const CLIENT_ID = ''
 export const CLIENT_SECRET = ''
