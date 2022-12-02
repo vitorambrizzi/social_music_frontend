a8c354278290faee7cbc86d5bc0c4b91cfcfb67e
Vítor Ambrizzi <ambrizzivitor@gmail.com>
2022-12-02 14:05:02 -0300

    Finished slugify function

--- src/helpers/url.js
+++ src/helpers/url.js
@@ -1,22 +1,36 @@
-const slugify = (input) => {
-	return input.toString().toLowerCase()
-  .replace(/[àáâãäåª]+/g, 'a')
-	.replace(/[èéêë]+/g, 'e')       	// Special Characters #2
-  .replace(/[ìíîï]+/g, 'i')       	// Special Characters #3
-	.replace(/[òóôõöº]+/g, 'o')       	// Special Characters #4
-  .replace(/[ùúûü]+/g, 'u')       	// Special Characters #5
-	.replace(/[ýÿ]+/g, 'y')       		// Special Characters #6
-	.replace(/[ñ]+/g, 'n')       			// Special Characters #7
-	.replace(/[ç]+/g, 'c')       			// Special Characters #8
-  .replace(/[ß]+/g, 'ss')       			// Special Characters #9
-	.replace(/[æ]+/g, 'ae')       			// Special Characters #10
-  .replace(/[øœ]+/g, 'oe')       		// Special Characters #11
-	.replace(/[%]+/g, 'pct')       			// Special Characters #12
-	.replace(/\s+/g, '-')           		// Replace spaces with -
-	.replace(/[^\w\-]+/g, '')       		// Remove all non-word chars
-  .replace(/\-\-+/g, '-')         		// Replace multiple - with single -
-	.replace(/^-+/, '')             		// Trim - from start of text
-	.replace(/-+$/, '');            		// Trim - from end of text
-};
+const slugify = (string) => {
+  const trimStart = new RegExp(/^\s+/, 'g')
+  const trimEnd = new RegExp(/\s+$/, 'g')
+  const space = new RegExp(/\s+/, 'g')
+  const hyphen = new RegExp(/-+/, 'g')
+  const sC01 = new RegExp(/[àáâãäåª]/, 'g')
+  const sC02 = new RegExp(/[èéêë]/, 'g')
+  const sC03 = new RegExp(/[ìíîï]/, 'g')
+  const sC04 = new RegExp(/[òóôõöº]/, 'g')
+  const sC05 = new RegExp(/[ùúûü]/, 'g')
+  const sC06 = new RegExp(/[ýÿ]/, 'g')
+  const sC07 = new RegExp(/[ñ]/, 'g')
+  const sC08 = new RegExp(/[ç]/, 'g')
+  const sC09 = new RegExp(/[ß]/, 'g')
+  const sC10 = new RegExp(/[æ]/, 'g')
+  const sC11 = new RegExp(/[øœ]/, 'g')
+  
+  return  string.toString().toLowerCase()
+  .replace(trimStart, '')
+  .replace(trimEnd, '')    
+  .replace(space, '+')
+  .replace(hyphen, '-')
+  .replace(sC01, 'a')
+  .replace(sC02, 'e')
+  .replace(sC03, 'i')
+  .replace(sC04, 'o')
+  .replace(sC05, 'u')
+  .replace(sC06, 'y')
+  .replace(sC07, 'n')
+  .replace(sC08, 'c')
+  .replace(sC09, 'ss')
+  .replace(sC10, 'ae')
+  .replace(sC11, 'oe')
+}
 
 export default slugify
