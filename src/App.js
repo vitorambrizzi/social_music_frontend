5a2f60ef89ae30835d4123cdc2b59e9b898aa12f
Vítor Ambrizzi <ambrizzivitor@gmail.com>
2022-12-08 17:43:04 -0300

    Changed files names

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from './layouts/Layout'
 import Home from './pages/Home'
-import Albums from './pages/Albums'
-import Artists from './pages/Artists'
-import Tracks from './pages/Tracks'
+import Album from './pages/Album'
+import Artist from './pages/Artist'
+import Track from './pages/Track'
 import Search from './pages/Search'
 
 function App() {
@@ -12,9 +12,9 @@ function App() {
       <Route path='/' element={<Layout />}>
         <Route index element={<Home />} />
         <Route path='search/:type/:query' element={<Search />} />
-        <Route path='album/:id' element={<Albums />} />
-        <Route path='artist/:id' element={<Artists />} />
-        <Route path='track/:id' element={<Tracks />} />
+        <Route path='album/:id' element={<Album />} />
+        <Route path='artist/:id' element={<Artist />} />
+        <Route path='track/:id' element={<Track />} />
       </Route>
     </Routes>
   );
