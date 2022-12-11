e1223f39a8ce06a6d93b529e3278fd1c216225e8
Vítor Ambrizzi <ambrizzivitor@gmail.com>
2022-12-11 11:19:00 -0300

    Added client id and secret to config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // Spotify API
-export const CLIENT_ID = ''
-export const CLIENT_SECRET = ''
+export const CLIENT_ID = 'b5a62d946c224d10a031a1f783f45ac6'
+export const CLIENT_SECRET = 'e2ef95c560c94fd7842a6b5d8fa11786'
 export const SPOTIFY_ACCOUNTS = 'https://accounts.spotify.com/api/token'
 export const SPOTIFY_API = 'https://api.spotify.com/v1'
