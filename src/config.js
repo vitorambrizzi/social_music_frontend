40ad92c33040df8b18548e6d165087c8c3ff617d
Vítor Ambrizzi <ambrizzivitor@gmail.com>
2022-12-11 19:29:50 -0300

    Added API BASE_PATH config

--- src/config.js
+++ src/config.js
@@ -3,3 +3,6 @@ export const CLIENT_ID = 'b5a62d946c224d10a031a1f783f45ac6'
 export const CLIENT_SECRET = 'e2ef95c560c94fd7842a6b5d8fa11786'
 export const SPOTIFY_ACCOUNTS = 'https://accounts.spotify.com/api/token'
 export const SPOTIFY_API = 'https://api.spotify.com/v1'
+
+// Social Music API
+export const BASE_PATH = 'http://localhost/social_music/backend/'
