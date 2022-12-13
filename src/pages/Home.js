d87e7f34fcc1f48aef99ab299b7b2c65291c08f2
Vítor Ambrizzi <ambrizzivitor@gmail.com>
2022-12-13 16:40:58 -0300

    Increased lorem ipsum placeholder at home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,6 +8,15 @@ const Home = () => {
       <p>
         Sed ultrices nisi metus, in blandit quam bibendum vitae. Aliquam a risus ac tellus ultrices dapibus tincidunt quis mauris. Cras fringilla porttitor erat. Sed non vestibulum libero. Sed mattis nulla ligula. Phasellus feugiat diam non turpis mattis convallis. Praesent vestibulum, risus vel porttitor malesuada, magna massa finibus neque, sed vestibulum mi ligula ut tellus. Sed congue nunc quis ex molestie, efficitur interdum felis tincidunt. In eu ante malesuada, vehicula felis ac, rhoncus elit. Donec pulvinar justo at purus pharetra, sed blandit urna congue. Phasellus placerat rutrum arcu id porta. In nec vehicula est. Aenean euismod dignissim dolor quis tempor. Aliquam id risus nulla.
       </p>
+      <p>
+      Nulla a cursus quam. Vestibulum quis euismod leo. Fusce eget tristique quam, ac vulputate ligula. Nullam ipsum ante, eleifend ut nunc sed, semper tincidunt arcu. Sed elementum maximus leo, at mattis tellus elementum et. Aenean varius egestas ligula non consectetur. Nam eget nisl ac arcu tincidunt posuere et a neque. Morbi ultrices gravida ornare. Nam suscipit quam ac mauris laoreet tincidunt. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam rhoncus aliquam mollis. Nam at ornare ex. In viverra maximus quam eget elementum. Quisque luctus ut felis id venenatis. Sed tempus orci sed libero facilisis, et tempor eros vehicula. Vestibulum nisi turpis, lacinia ac sodales non, ultrices et leo.
+      </p>
+      <p>
+      Proin bibendum, lectus ac porta tristique, nunc mi dignissim diam, nec condimentum nulla risus eu magna. Donec et justo hendrerit, tempus quam vel, rhoncus orci. Proin justo erat, porttitor quis placerat ullamcorper, lacinia non nisi. Maecenas convallis arcu vel risus tempus lobortis. Donec nec commodo libero. Phasellus finibus quam ac ipsum viverra, et faucibus mi eleifend. Suspendisse feugiat pharetra risus non mattis. Praesent porta lectus ex, sit amet aliquam est elementum id. Aliquam non pulvinar mauris, ac consectetur nisi. Ut fringilla ex quis tortor luctus semper. Cras id sem suscipit, gravida metus eu, venenatis lacus. Cras ac tellus et augue pharetra mattis quis nec velit. Aliquam maximus vel risus mollis faucibus. Ut vel tellus egestas, cursus turpis ut, auctor sem. Suspendisse potenti. Morbi vel leo sit amet dui tempus eleifend.
+      </p>
+      <p>
+      Mauris ac dolor vitae sapien faucibus rhoncus. Donec elementum turpis vitae mi tincidunt, vel hendrerit lacus consequat. Donec ornare justo et velit pretium, et posuere velit feugiat. Praesent vitae nunc vel nisl varius dapibus. Cras vitae rutrum quam. Nulla suscipit porta auctor. Praesent mollis pulvinar risus, a semper diam viverra at.
+      </p>
     </>
   )
 }
